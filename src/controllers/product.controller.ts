7d39ead77f4e8cab9ec63cc36c4ad6921cc36863
dmlen <dmlen@rollncode.com>
2023-11-26 21:29:28 +0200

    [TASK01] feat: implement Product thumbnail POST and DEL routes
    – Implement POST route for uploading or updating a Product thumbnail
    – Implement DEL route for deleting a Product thumbnail

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,3 +1,6 @@
+import { exists, mkdir, writeFile, unlink } from 'fs';
+import { promisify } from 'util';
+import path from 'path';
 import {
   Count,
   CountSchema,
@@ -15,13 +18,33 @@ import {
   put,
   del,
   requestBody,
-  response,
+  HttpErrors,
+  Request, response, RestBindings, Response,
 } from '@loopback/rest';
+import {inject} from '@loopback/core';
+import sharp from 'sharp';
 import {Product} from '../models';
 import {ProductRepository} from '../repositories';
+import {FILE_UPLOAD_SERVICE} from '../keys';
+import {FileUploadHandler} from '../types';
+
+const writeFileAsync = promisify(writeFile);
+const existsAsync = promisify(exists);
+const mkDirAsync = promisify(mkdir);
+const unlinkAsync = promisify(unlink);
+
+const MAX_FILE_SIZE_MB = 1;
+const MAX_FILE_SIZE = MAX_FILE_SIZE_MB * 1024 * 1024;
+
+const fileDirOnServer = 'uploads/product-thumbnails';
+const publicDir = path.join(__dirname, '../../public');
+const uploadThumbnailDirPath = path.join(publicDir, fileDirOnServer);
+
+const makeThumbnailFilename = (id: string) => `${id}_thumbnail_${Date.now()}.jpg`
 
 export class ProductController {
   constructor(
+    @inject(FILE_UPLOAD_SERVICE) private handler: FileUploadHandler,
     @repository(ProductRepository)
     public productRepository : ProductRepository,
   ) {}
@@ -146,5 +169,135 @@ export class ProductController {
   })
   async deleteById(@param.path.string('id') id: string): Promise<void> {
     await this.productRepository.deleteById(id);
+  }
+
+
+  @post('/products/{id}/thumbnail')
+  @response(200, {
+    description: 'Product model instance',
+  })
+  async uploadThumbnail(
+    @param.path.string('id') id: string,
+    @requestBody.file() req: Request,
+    @inject(RestBindings.Http.RESPONSE) res: Response,
+  ): Promise<string> {
+    // Ensure that the product with the given id exists
+    const product = await this.productRepository.findById(id);
+    if (!product) {
+      throw new HttpErrors.NotFound(`Product with id ${id} not found.`);
+    }
+
+
+    // Enrich the request with the files body
+    try {
+      await new Promise<void>((resolve, reject) => {
+        // @ts-ignore
+        this.handler(req, res, (err: unknown) => {
+          if (err) reject(err);
+          resolve();
+        })
+      });
+    } catch (err) {
+      throw new HttpErrors.InternalServerError(err.toString());
+    }
+
+
+    // Define file from the request body
+    // @ts-ignore
+    const uploadedFiles = req.files;
+    const files: Express.Multer.File[] = []
+    if (Array.isArray(uploadedFiles)) {
+      files.push(...uploadedFiles);
+    } else {
+      for (const filename in uploadedFiles) {
+        files.push(uploadedFiles[filename]);
+      }
+    }
+    if (!files.length) {
+      throw new HttpErrors.BadRequest('Invalid file data');
+    }
+    const file = files[0];
+
+
+    // File validation
+    if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
+      throw new HttpErrors.BadRequest('Only JPG and PNG files are allowed');
+    }
+    if (file.size > MAX_FILE_SIZE) {
+      throw new HttpErrors.BadRequest(`File size should not exceed ${MAX_FILE_SIZE_MB}MB`);
+    }
+
+
+    // Create the upload directory if not exists
+    if (!(await existsAsync(uploadThumbnailDirPath))){
+      await mkDirAsync(uploadThumbnailDirPath, { recursive: true });
+    }
+
+
+    // Delete the previous thumbnail file if exists
+    if (product.thumbnail) {
+      const filePath = path.join(publicDir, product.thumbnail)
+      try {
+        await unlinkAsync(filePath)
+      } catch (e) {
+        console.log(`Error while deleting previous thumbnail file ${filePath}`, e)
+      }
+    }
+
+
+    // Save the file to the local file system
+    const fileName = makeThumbnailFilename(id)
+    const filePath = path.join(uploadThumbnailDirPath, fileName)
+    const fileUrl = path.join(fileDirOnServer, fileName)
+
+    const imageResizedBuffer = await sharp(files[0].buffer)
+      .resize(100, 100)
+      .toBuffer();
+    await writeFileAsync(filePath, imageResizedBuffer);
+
+
+    // Update the product with the new thumbnail
+    product.thumbnail = fileUrl;
+    await this.productRepository.update(product);
+
+
+    return fileUrl;
+  }
+
+  @del('/products/{id}/thumbnail')
+  @response(204, {
+    description: 'Product thumbnail DELETE success',
+  })
+  async deleteThumbnail(
+    @param.path.string('id') id: string,
+  ): Promise<void> {
+    // Ensure that the product with the given id exists
+    const product = await this.productRepository.findById(id);
+    if (!product) {
+      throw new HttpErrors.NotFound(`Product with id ${id} not found.`);
+    }
+
+
+    // Check if the product has a thumbnail to delete
+    if (!product.thumbnail) {
+      return;
+    }
+
+
+    // Delete the file from the local file system
+    const filePath = path.join(uploadThumbnailDirPath, product.thumbnail);
+    try {
+      await unlinkAsync(filePath);
+    } catch (e) {
+      console.log(`Error while deleting file ${filePath}`, e)
+    }
+
+
+    // Clear the previewUrl property of the product
+    product.thumbnail = '';
+
+
+    // Update the product in the database
+    await this.productRepository.update(product);
   }
 }
