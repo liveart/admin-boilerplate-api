fe4c790984435e7dc7c115f55bd98f619ca665d6
dmlen <dmlen@rollncode.com>
2023-11-26 21:22:09 +0200

    [TASK01] feat: implement service "FileUpload"
    – "FileUpload" service is needed to parse incoming request's FormData body. It's used to extract an uploading thumbnail file and store it locally on the server file system for the further serving.

---
+++ src/keys.ts
@@ -0,0 +1,9 @@
+import {BindingKey} from '@loopback/core';
+import {FileUploadHandler} from './types';
+
+/**
+ * Binding key for the file upload service
+ */
+export const FILE_UPLOAD_SERVICE = BindingKey.create<FileUploadHandler>(
+  'services.FileUpload',
+);
